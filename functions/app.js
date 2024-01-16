1f7e6676b746ca2da1f818fa1245e6c17f81df84
Briefgarde <nemo.vollert@he-arc.ch>
2024-01-16 12:13:56 +0100

    allowed the real-time website to fetch more info on sales from the DB

--- functions/app.js
+++ functions/app.js
@@ -1,12 +1,25 @@
+/* eslint-disable no-unused-vars */
 const express = require("express");
 const {PubSub} = require("@google-cloud/pubsub");
 const app = express();
 const pubSubClient = new PubSub();
-const subscriptionName = "projects/codepetzi/subscriptions/webhookPetzi-sub"; // replace with your subscription name
+const subscriptionName = "projects/codepetzi/subscriptions/webhookPetzi-sub";
+//  ----------Part 2: Firestore----------------
+const {initializeApp, applicationDefault, cert} = require("firebase-admin/app");
+const {getFirestore, Timestamp, FieldValue, Filter} = require("firebase-admin/firestore");
+
+const serviceAccount = require("../keys/keysFirestore.json");
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
+//  ------------------------------------------
 
 const clients = [];
 
-app.use(express.static("website")); // serve your HTML file
+app.use(express.static("website"));
 
 app.get("/events", (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
@@ -18,9 +31,16 @@ let count = 0;
 
 const messageHandler = (message) => {
   count += 1;
-  clients.forEach((res) =>
-    res.write(`data: ${JSON.stringify({count: count, message: message.data.toString()})}\n\n`),
-  );
+  clients.forEach(async (res) =>{
+    // get ticket number from json
+    const ticketNumber = JSON.parse(message.data.toString()).ticketNumber;
+    const cityRef = db.collection("webhookPetzi").doc(ticketNumber);
+    const result = await cityRef.get();
+    console.log(result.data().ticketInfo);
+
+
+    res.write(`data: ${JSON.stringify({count: count, message: result.data().ticketInfo})}\n\n`);
+  });
   message.ack();
 };
 
